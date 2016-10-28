5969ad607c84d17b21cb2a52a4ba8cf15be98f8a
JbIPS <jeanbaptiste.richardet@gmail.com>
2016-10-28 11:30:48 +0200

    Adds full capabilities options
    See https://www.browserstack.com/automate/node

--- src/browserstack/Capabilities.hx
+++ src/browserstack/Capabilities.hx
@@ -2,9 +2,13 @@ package browserstack;
 
 typedef Capabilities = {
   browserName:BrowserName,
+  browser_version: String,
   browserstack: {
     user:String,
     key:String,
     local:Bool,
-  }
+  },
+  os: String,
+  os_version: String,
+  resolution: String
 }
